74dc1dfa84920e2ecf73befd6101d96eddc68338
thibaudrolain-olympp <thibaud.rolain@gmail.com>
2025-10-17 13:20:18 +0000

    add tests to front

--- MarketPlaceFront/src/app/guards/auth-guard-guard.spec.ts
+++ MarketPlaceFront/src/app/guards/auth-guard-guard.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { authGuardGuard } from './auth-guard-guard';
+import { AuthGuard } from './auth-guard-guard';
 
 describe('authGuardGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuardGuard(...guardParameters));
+      TestBed.runInInjectionContext(() => AuthGuard(...guardParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
--- MarketPlaceFront/src/app/pages/auth-layout/auth-layout.spec.ts
+++ MarketPlaceFront/src/app/pages/auth-layout/auth-layout.spec.ts
@@ -1,18 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { AuthLayout } from './auth-layout';
+import { AuthLayoutComponent } from './auth-layout';
 
 describe('AuthLayout', () => {
-  let component: AuthLayout;
-  let fixture: ComponentFixture<AuthLayout>;
+  let component: AuthLayoutComponent;
+  let fixture: ComponentFixture<AuthLayoutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AuthLayout]
+      imports: [AuthLayoutComponent, RouterTestingModule]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AuthLayout);
+    fixture = TestBed.createComponent(AuthLayoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
